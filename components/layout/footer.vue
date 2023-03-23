c0d511c3e4b1711260259542ed93ddf7f5e4edb8
Valentina-Olekhnovich <volekh@gmail.com>
2023-03-23 21:41:18 +0100

    fix: footer cv link

--- components/layout/footer.vue
+++ components/layout/footer.vue
@@ -137,7 +137,7 @@
             </div>
             <div class="mt-12 md:mt-0">
               <a
-                href="../../public/valentina.olekhnovich.cv.pdf"
+                href="/valentina.olekhnovich.cv.pdf"
                 download
                 class="whitespace-nowrap hover:text-gray-00 -mt-2 inline-flex items-center justify-end border border-gray-400 bg-slate-800 px-4 py-2 text-base font-medium text-gray-400 shadow-sm hover:bg-slate-700 hover:text-gray-300"
               >
